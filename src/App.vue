12ae8183ad51188664e6ba6c14d33a3118bef535
Jalil <abdullacelil00@gmail.com>
2024-10-16 06:00:00 +0400

    Create components

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,23 @@
 <template>
-  My App
+  <Header/>
+  <div class="container">
+    <Balance/>
+    <IncomeExpenses/>
+    <TransactionList/>
+    <AddTransaction/>
+  </div>
 </template>
+
+<script>
+
+import {defineComponent} from "vue";
+import Header from "@/components/Header.vue";
+import Balance from "@/components/Balance.vue";
+import IncomeExpenses from "@/components/IncomeExpenses.vue";
+import TransactionList from "@/components/TransactionList.vue";
+import AddTransaction from "@/components/AddTransaction.vue";
+
+export default defineComponent({
+  components: {AddTransaction, TransactionList, IncomeExpenses, Balance, Header}
+})
+</script>
